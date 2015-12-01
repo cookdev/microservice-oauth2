c272ac4a130e56c81021b9c10675d342ab68240f
no1hsk <jsthahn@naver.com>
2015-12-02 01:04:00 +0900

    part6

---
+++ part6/sample-resource-server/src/main/java/org/anyframe/cloud/auth/SampleResourceServerApplication.java
@@ -0,0 +1,29 @@
+package org.anyframe.cloud.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootApplication
+@RestController
+@EnableRedisHttpSession
+public class SampleResourceServerApplication {
+
+    @RequestMapping("/")
+    public Map<String,Object> home() {
+        Map<String,Object> model = new HashMap<String,Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleResourceServerApplication.class, args);
+    }
+}
